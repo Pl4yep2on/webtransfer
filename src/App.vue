b80807cc3c0ffcc4d9008deffa3d5e62e1dcccd7
GMrrc <143954951+GMrrc@users.noreply.github.com>
2024-11-28 15:04:30 +0100

    padding of webcontent view

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
 		<div class="h-full w-full flex flex-col sm:flex-row">
 			<!-- Première section -->
 			<div 
-				class="w-full sm:w-1/3 max-sm:h-2/5 p-4 sm:m-6 rounded-xl bg-NcBlack/40">
+				class="w-full sm:w-1/3 max-sm:h-2/5 p-4 sm:m-6 sm:mr-0 rounded-xl bg-NcBlack/40">
 				<WebContentViewer @zip-upload="handleZipUpload" @file-upload="handleFileUpload" zipUrl="http://localhost:8000/dummyZip.zip"/>
 			</div>
 			<!-- Deuxième section -->
 			<div 
-				class="w-full sm:w-2/3 max-sm:h-3/5 p-4 sm:m-6 bg-NcBlack rounded-xl">
+				class="w-full sm:w-2/3 max-sm:h-3/5 p-4 sm:m-6 sm:ml-4 bg-NcBlack rounded-xl">
 				<FileTable :file="sharedFile" :zip="zip"/>
 			</div>
 		</div>
